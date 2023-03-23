8b4d8155868517ac8c23e9c65ea6e1210e4cf9ae
Kusumkar Suyash <kusumkarsuyash1234@gmail.com>
2023-03-24 01:51:34 +0530

    Commit

---
+++ app/src/main/java/com/suyash/assignment/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.suyash.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
